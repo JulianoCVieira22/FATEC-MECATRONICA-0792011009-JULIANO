a7b9a1f5238ceedaf3f63f4f76f489c98480b10f
JulianoCVieira22 <62109645+JulianoCVieira22@users.noreply.github.com>
2020-08-27 10:27:14 -0300

    Create programa01.py

---
+++ LTP2-2020-2/Pratica02/programa01.py
@@ -0,0 +1,9 @@
+#Função para escrever na tela:
+print("Ola mundo")
+print('Bom Dia')
+#Função de Leitura do Teclado:
+input("Digite seu numero")
+'''
+Isso é um boloco de comentarios
+'''
+print("Bom dia",nome, "!")
